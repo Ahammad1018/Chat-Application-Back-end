c53eb6fdad065d61e61507cae88aaaca65c9df0b
Ahammad1018 <ahammadali1018@gmail.com>
2025-06-24 12:38:18 +0530

    chat application back-end

--- src/main/java/com/example/chat/config/WebConfig.java
+++ src/main/java/com/example/chat/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://chat-application-front-end-80j9.onrender.com") //,"http://localhost:5173") // React app URL
+                .allowedOrigins("https://chat-application-front-end-80j9.onrender.com", "http://localhost:5173") // React app URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type", "X-XSRF-TOKEN")
                 .allowCredentials(true); // Allow credentials if needed (e.g., authentication cookies)
